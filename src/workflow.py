e5a574df15240e6168014a3fd3812e91cf1fc4e4
Yash Kushwaha <yash1158@gmail.com>
2025-07-11 16:29:48 +0530

    simple workflow is implemented however the inference is again happening on the cpu instead of gpu. Let's remove the async key and see if that is the reason why inference dont happen on GPU

--- src/workflow.py
+++ src/workflow.py
@@ -1,6 +1,7 @@
-from src.tools import ImageRetrieverTool
+from src.tools import ImageRetrieverTool, prepare_images_for_prompt
 from src.image_retriever import ImageRetriever
 
+from jinja2 import Template
 from llama_index.core.workflow import (
     Event,
     StartEvent,
@@ -9,34 +10,67 @@ from llama_index.core.workflow import (
     step,
 )
 
+from src.ollama_vision import  OllamaVisionLLM    
+
+
+QUERY_TEMPLATE = """
+You are a helpful assistant. Your job is to evaluate the relevance of a list of images to the user query.
+
+Images are going to be in the format <image_num> : <image_id> e.g.
+<image_1> : 123.jpg
+<image_2> : 456.jpg
+<image_3> : abc.jpg
+
+Identify the most relevant image and also write a short description of the image.
+Output in json format only using the format:
+{"best_image":"456.jpg",
+ "description": "Description of the image"
+}
+
+USER QUERY:
+{{user_query}}
+
+IMAGES:
+{{images}}
+
+"""
+template = Template(QUERY_TEMPLATE)
+
+
 class RetrieveImageListEvent(Event):
     image_list: list
 
 
 class ImageRetrievalWorkfow(Workflow):
-    def __init__(self, image_retriever_tool):
+    def __init__(self, image_retriever_tool, vision_llm):
         super().__init__()
         self.image_retriever_tool = image_retriever_tool
-
+        self.template = Template(QUERY_TEMPLATE)
+        self.vision_llm = vision_llm
     @step
     async def retrieve_image_ids(self, ev: StartEvent) -> RetrieveImageListEvent:
         query = ev.query
         results = self.image_retriever_tool(query) 
         images = [node.text for node in results]
-        return RetrieveImageListEvent(image_list=images)
+        return RetrieveImageListEvent(image_list=images, query = query)
     
     @step
-    async def show_image_ids(self, ev: RetrieveImageListEvent) -> StopEvent:
-        for i in ev.image_list:
-            print(i)
-        return StopEvent()
+    async def evaluate_images(self, ev: RetrieveImageListEvent) -> StopEvent:
+        image_string, base64_images = prepare_images_for_prompt(ev.image_list)
+        prompt = self.template.render(user_query = ev.query, images = image_string)    
+        response_gen = self.vision_llm.stream_complete(prompt=prompt, image_source=base64_images)
+        final_response = ""
+        for chunk in response_gen:
+            print(chunk.delta, end="", flush=True)  # Print tokens as they stream
+            final_response += chunk.delta
+        return StopEvent(final_response=final_response)
 
-async def main(image_retriever_tool):
-    w = ImageRetrievalWorkfow(image_retriever_tool)
-    result = await w.run(query='Hello, World !')
+
+async def main(image_retriever_tool, vision_llm):
+    
+    w = ImageRetrievalWorkfow(image_retriever_tool, vision_llm)
+    result = await w.run(query='A typical busy day in a big metro city')
     print(result)
-
-
 
 if __name__ == '__main__':
     from src.embedding_client import RemoteEmbedding
@@ -51,8 +85,8 @@ if __name__ == '__main__':
     image_retriever = ImageRetriever(embed_model=embed_model, vec_store=vec_store, similarity_top_k=4)
     image_retriever_tool = ImageRetrieverTool(image_retriever=image_retriever)
     import asyncio
-
-    asyncio.run(main(image_retriever_tool))
+    vision_llm = OllamaVisionLLM(model_name = 'gemma3:12b')
+    asyncio.run(main(image_retriever_tool, vision_llm))
     
 
 
